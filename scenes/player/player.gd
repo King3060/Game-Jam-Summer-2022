1f1bb44f28df54bd484c938809a9184237043b5a
Emily <tweetychrip@gmail.com>
2022-09-26 15:02:26 -0700

    add move animation

--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -1,9 +1,9 @@
 extends KinematicBody2D
 
 
-export var speed := 200
+export var speed := 150
 export var gravity := 1_000
-export var jump_force := 800
+export var jump_force := 400
 
 var velocity := Vector2()
 
@@ -15,3 +15,8 @@ func _physics_process(delta: float) -> void:
 	if Input.is_action_pressed("move_up") and is_on_floor():
 		velocity.y = -jump_force
 	velocity = move_and_slide(velocity, Vector2.UP)
+	if input != 0:
+		$AnimationPlayer.play("move")
+		$Sprites.scale.x = sign(input)
+	else:
+		$AnimationPlayer.play("RESET")
